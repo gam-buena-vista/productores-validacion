c7d0e3c5707bc5a6fb33d8842b46366d61ebf4ef
gam-buena-vista <gambv1710@gmail.com>
2025-04-16 11:25:38 -0400

    Update productores.js

--- productores.js
+++ productores.js
@@ -1,6 +1,4 @@
 const productores = {
 "BV-D1-001": {nombre: "MARINA  CHORÉ ZEBALLOS", ci: "6253862", distrito: "SANTA BÁRBARA DE LOS MANZANOS", estado: "Afiliado válido"},
 "BV-D1-002": {nombre: "MARIA CRISTINA DAZA MOSTAZEDA", ci: "3911789", distrito: "SANTA BÁRBARA DE LOS MANZANOS", estado: "Afiliado válido"},
-
-
-  };
+};
