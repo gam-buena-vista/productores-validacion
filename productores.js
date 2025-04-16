fe36379911464529b35a86e8176dd7a3a3fb6c91
gam-buena-vista <gambv1710@gmail.com>
2025-04-16 01:20:59 -0400

    Add files via upload

---
+++ productores.js
@@ -0,0 +1,57 @@
+const productores = {
+    "BV-D6-001": {
+      nombre: "TEODORO OLIVERA APAZA",
+      ci: "2986416 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-002": {
+      nombre: "VICTOR SALVADOR LOPEZ",
+      ci: "2744085 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-003": {
+      nombre: "EUFRACIO QUISPE PEREZ",
+      ci: "4258853 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-004": {
+      nombre: "GUILLERMO SEJAS OCHALI",
+      ci: "7690471 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-005": {
+      nombre: "GRETA ALIVERA SEJAS",
+      ci: "7803696 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-006": {
+      nombre: "ELISEO ZURITA FLORES",
+      ci: "7711670 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-007": {
+      nombre: "EMETERIO SEJAS ROCHA",
+      ci: "5419120 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-008": {
+      nombre: "LICEO AMURRIO COCA",
+      ci: "9004092 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    "BV-D6-009": {
+      nombre: "MARTIN MARTINEZ ACUÑA",
+      ci: "4647607 SC",
+      distrito: "Distrito 6 - Carmen Surutu",
+      estado: "Afiliado válido"
+    },
+    // Agrega los demás registros aquí...
+  };
