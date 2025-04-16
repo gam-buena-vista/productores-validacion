5a23cf85d098ab34cc83a3ac6ff82e14dcd2a16b
gam-buena-vista <gambv1710@gmail.com>
2025-04-16 02:21:15 -0400

    Update productores.js

--- productores.js
+++ productores.js
@@ -42,7 +42,7 @@ const productores = {
       estado: "Afiliado válido"
     },
     "BV-D6-008": {
-      nombre: "ELICEO AMURRIO COCA",
+      nombre: "LICEO AMURRIO COCA",
       ci: "9004092 SC",
       distrito: "Distrito 6 - Carmen Surutu",
       estado: "Afiliado válido"
@@ -53,5 +53,318 @@ const productores = {
       distrito: "Distrito 6 - Carmen Surutu",
       estado: "Afiliado válido"
     },
+    "BV-D6-010": {
+        "nombre": "FABIAN SEJAS GUTIERREZ",
+        "ci": "9685740 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-011": {
+        "nombre": "JHONNY GALEAN VARGAS",
+        "ci": "5838652 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-012": {
+        "nombre": "ABELINO LUNA FLORES",
+        "ci": "4484792 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-013": {
+        "nombre": "APARICIO GUTIERREZ",
+        "ci": "8107124 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-014": {
+        "nombre": "ANGEL OLIVERA",
+        "ci": "5269147 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-015": {
+        "nombre": "MARCELINA OLIVERA",
+        "ci": "12730873 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-016": {
+        "nombre": "BERNARDO GUTIERREZ",
+        "ci": "8068661 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-017": {
+        "nombre": "MARINA GUTIERREZ POMA",
+        "ci": "7809817 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-018": {
+        "nombre": "CARMEN DANIELA MERCADO",
+        "ci": "8971899 SC",
+        "distrito": "Distrito 6 - Carmen Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-019": {
+        "nombre": "MARCO CONDORI HUARACHI",
+        "ci": "1946164 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-020": {
+        "nombre": "WALTER GUTIERREZ RELUZ",
+        "ci": "11374006 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-021": {
+        "nombre": "FRANCISCO RELUZ VALENCIA",
+        "ci": "8174939 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-022": {
+        "nombre": "GUADALUPE RELUZ DE HINOJOSA",
+        "ci": "8106586 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-023": {
+        "nombre": "TOMAS OLIVERA MONTAÑO",
+        "ci": "7674040 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-024": {
+        "nombre": "GREGORIO SEJAS ROCHA",
+        "ci": "9661587 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-025": {
+        "nombre": "FRANCISCO GUIZADA LOZA",
+        "ci": "4672920 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-026": {
+        "nombre": "DELIO CHOQUE LUNA",
+        "ci": "1854795 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-027": {
+        "nombre": "ISABEL SEJAS OLIVERA",
+        "ci": "11365972 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-028": {
+        "nombre": "JAIME FERNANDEZ TAPIA",
+        "ci": "8222565 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-029": {
+        "nombre": "IGNACIO MAMANI",
+        "ci": "RODRIGUEZ SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-030": {
+        "nombre": "LUZ ADELA COCA CABRERA",
+        "ci": "7667876 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-031": {
+        "nombre": "JULIAN OLIVERA APAZA",
+        "ci": "2919441 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-032": {
+        "nombre": "EXALTACIÓN GARCIA VASQUEZ",
+        "ci": "3943724 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-033": {
+        "nombre": "JOSÉ LUIS SEJAS VASQUEZ",
+        "ci": "7803694 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-034": {
+        "nombre": "EMIGDIO SEJAS RODRIGUEZ",
+        "ci": "4772874 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-035": {
+        "nombre": "GERMAN TENORIO COLICHO",
+        "ci": "2955294 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-036": {
+        "nombre": "ALEX GABRIEL MENDOZA TERCEROS",
+        "ci": "12478451 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-037": {
+        "nombre": "BRAULIO MANDOZA TERCEROS",
+        "ci": "9823728 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-038": {
+        "nombre": "JUANITO MENDOZA YUCRA",
+        "ci": "8911525 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-039": {
+        "nombre": "ESTELA FRANCO LÓPEZ",
+        "ci": "8957057 SC",
+        "distrito": "Distrito 6 - Carmen Surutu",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-040": {
+        "nombre": "ALEJANDRO SULLCA QUIÑONES",
+        "ci": "3948869 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-041": {
+        "nombre": "ENRIQUETA MORALES MAITA",
+        "ci": "7691294 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-042": {
+        "nombre": "JUAN SULLCA ACUÑA",
+        "ci": "3950187 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-043": {
+        "nombre": "WALBERTO SULLCA LABAYEN",
+        "ci": "14593121 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-044": {
+        "nombre": "ALFREDO PARDO RIOS",
+        "ci": "7690974 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-045": {
+        "nombre": "LUCAS JUCUMARI POMA",
+        "ci": "6224212 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-046": {
+        "nombre": "CESILIO ROMERO VILA",
+        "ci": "6335510 SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-047": {
+        "nombre": "BISMAR IRAÑA",
+        "ci": "PASTOR SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-050": {
+        "nombre": "WILBER IRAÑA",
+        "ci": "PASTOR SC",
+        "distrito": "Distrito 6 - San Juan De Saguayo",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-055": {
+        "nombre": "ELENA QUIROS DE RELUZ",
+        "ci": "6272379 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-056": {
+        "nombre": "SIMÓN GUIZADA CARTAGENA",
+        "ci": "8120477 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-057": {
+        "nombre": "PAULINA HUAINOCA SALAZAR",
+        "ci": "6240041 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-058": {
+        "nombre": "EUGENIO RELUZ ALMANZA",
+        "ci": "6325073 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-059": {
+        "nombre": "AURELIA SANIZO QUISPE",
+        "ci": "5311021 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-060": {
+        "nombre": "EDUARDO RELUZ VALENCIA",
+        "ci": "6240042 SC",
+        "distrito": "Distrito 6 - San Jorge",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-061": {
+        "nombre": "SANTOS LORA MOSCOSO",
+        "ci": "9004090 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-062": {
+        "nombre": "LIDIA LOPEZ PAREDES",
+        "ci": "7749157 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-063": {
+        "nombre": "ARMIN JALDIN CLAROS",
+        "ci": "6396730 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-064": {
+        "nombre": "CANDELARIA SOSA VACA",
+        "ci": "3191881 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-065": {
+        "nombre": "RENAN YLLESCAS GONZALES",
+        "ci": "1886715 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-066": {
+        "nombre": "MANUELA CUELLAR BAZÁN",
+        "ci": "4171784 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },
+      "BV-D6-067": {
+        "nombre": "MATILDE YUCRA QUISPE",
+        "ci": "5897679 SC",
+        "distrito": "Distrito 6 - San Juan Surutú",
+        "estado": "Afiliado válido"
+      },    
+
     // Agrega los demás registros aquí...
   };
