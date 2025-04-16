222c375577d899ef8b51db61d96692ec083cc198
gam-buena-vista <gambv1710@gmail.com>
2025-04-16 10:17:07 -0400

    Update productores.js

--- productores.js
+++ productores.js
@@ -1,4 +1,5 @@
 const productores = {
+     // DISTRITO 6 CARMEN SURUTU
     "BV-D6-001": {
       nombre: "TEODORO OLIVERA APAZA",
       ci: "2986416 SC",
@@ -400,7 +401,256 @@ const productores = {
         "ci": "5897679 SC",
         "distrito": "Distrito 6 - San Juan Surutú",
         "estado": "Afiliado válido"
-      },    
+      },   
+    
+    // DISTRITO 5 ARBOLEDA
+    {
+  "BV-D5-001": {
+    "nombre": "EDITH HERRERA DE VARGAS",
+    "ci": "1593770",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-002": {
+    "nombre": "JULIO EFRAIN JEREZ CRUZ",
+    "ci": "13107180",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-003": {
+    "nombre": "INGRID MOLE COIMBRA",
+    "ci": "10805090",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-004": {
+    "nombre": "EDITHA RODRIGUEZ DAZA",
+    "ci": "5836996",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-005": {
+    "nombre": "MAURICIO PUTARÉ CUASASE",
+    "ci": "16878742",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-006": {
+    "nombre": "GEERT HANS TIEMEN HOEKSTRA",
+    "ci": "E-0027255",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-007": {
+    "nombre": "TITO SANDOVAL GUZMAN",
+    "ci": "2857060",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-008": {
+    "nombre": "CRISOSTOMO AVILA CARVAJAL",
+    "ci": "1940217",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-009": {
+    "nombre": "ISAAC SALVATIERRA MONTENEGRO",
+    "ci": "1952280",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-010": {
+    "nombre": "JORGE GALLARDO RODRIGUEZ",
+    "ci": "4551589-1C",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-011": {
+    "nombre": "CONSTANSA SOSA",
+    "ci": "3267094",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-012": {
+    "nombre": "SANTOS PARADA RIVERO",
+    "ci": "8210790",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-013": {
+    "nombre": "ARTURO VARGAS ARAUZ",
+    "ci": "5856242",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-014": {
+    "nombre": "GIL ANTONIO SALVATIERRA MONTENEGRO",
+    "ci": "1996915",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-015": {
+    "nombre": "ESTANISLAO HUGO SOSA URRUTIA",
+    "ci": "4698973-1I",
+    "distrito": "ARBOLEDA",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-016": {
+    "nombre": "JUANA MAMANI DE ASILLO",
+    "ci": "2969353",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-017": {
+    "nombre": "YENNY CONTRERAS MACEDO",
+    "ci": "7787586",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-018": {
+    "nombre": "JAIME SOTILLO FLORES",
+    "ci": "7746876",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-019": {
+    "nombre": "DANIEL SOTILLO ISLA",
+    "ci": "9054196",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-020": {
+    "nombre": "PEDRO SOTILLO IRAÑA",
+    "ci": "1947935",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-021": {
+    "nombre": "GLADY ONOFRE ROMAN",
+    "ci": "8244470",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-022": {
+    "nombre": "ZENOBIA MACEDO",
+    "ci": "3423409",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-023": {
+    "nombre": "JULIAN PUCHO YUCRA",
+    "ci": "4088205",
+    "distrito": "EL PALMAR",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-024": {
+    "nombre": "MIGUEL ANGEL AVILA VACA",
+    "ci": "7803605",
+    "distrito": "SAN MIGUELITO",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-025": {
+    "nombre": "VILMA VACA ZEBALLOS",
+    "ci": "9051453",
+    "distrito": "SAN MIGUELITO",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-026": {
+    "nombre": "EDWIN MANUEL GAMARRA PEÑAFIEL",
+    "ci": "3218764",
+    "distrito": "PALACIOS",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-027": {
+    "nombre": "NELSON LORA ARAMAYO",
+    "ci": "6361788",
+    "distrito": "PALACIOS",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-028": {
+    "nombre": "FAVIANA COSSIO VARGAS",
+    "ci": "6529222",
+    "distrito": "PALACIOS",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-029": {
+    "nombre": "MARCO ANTONIO OLIVA GUTIERREZ",
+    "ci": "7803622",
+    "distrito": "PALACIOS",
+    "estado": "Afiliado válido"
+  },
+  "BV-D5-030": {
+    "nombre": "HORMANDO OLIVA GUTIERREZ",
+    "ci": "7803639",
+    "distrito": "PALACIOS",
+    "estado": "Afiliado válido"
+  },
+    // DISTRITO 4 CARANDA
+    
+    
+    // DISTRITO 3 ESPEJITOS
 
-    // Agrega los demás registros aquí...
+    
+    // DISTRITO 2 HUAYTÚ
+    {
+  "BV-D2-001": { nombre: "LORGIO JUSTINIANO CAMARGO", ci: "3174924", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-002": { nombre: "GONZALO RODRIGUEZ DIAS", ci: "4686826", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-003": { nombre: "AVELINO RODRIGUEZ MENDIETA", ci: "3770486", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-004": { nombre: "JUAN WALTER IBAÑEZ CARI", ci: "2972612", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-005": { nombre: "NELSON RUDI IBAÑEZ VOCAL", ci: "9806259", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-006": { nombre: "JESUS FERNANDEZ FLORES", ci: "4590284", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-007": { nombre: "FRANZ REYNALDO CALA", ci: "8187156", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-008": { nombre: "SIXTO LLAVE FRANCO", ci: "8163857", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-009": { nombre: "PONCIANO SANDOVAL MORALES", ci: "6293239", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-010": { nombre: "TOMAS CALIZAYA MENDOZA", ci: "3224289", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-011": { nombre: "DOMINGO BARRIOS VILLARPANDO", ci: "7827564", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-012": { nombre: "FELIX YAPO MAMANI", ci: "2972594", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-013": { nombre: "BENJAMIN LOAYZA CALIZAYA", ci: "17770710", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-014": { nombre: "JOSE ARIEL SILES ORTEGA", ci: "5828754", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-015": { nombre: "CELESTINO HUAYNOCA TOLA", ci: "3749445", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-016": { nombre: "ESTHER FAJARDO CRUZ", ci: "3936858", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-017": { nombre: "ANTONIO BASCOPE HIDALGO", ci: "2900692", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-018": { nombre: "FORTUNATO SANDOVAL", ci: "2972762", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-019": { nombre: "MANUEL MONTERO TOMICHA", ci: "2955692", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-020": { nombre: "JULIANA MENESES SACACA", ci: "9696142", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-021": { nombre: "ERLINDA ZURITA CESPEDEZ", ci: "4648791", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-022": { nombre: "WILSON MEJIA HERRERA", ci: "5394954", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-023": { nombre: "SANTUSA SANDOVAL FRANCO", ci: "9711545", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-024": { nombre: "LICET CARDENAS DOMINGUEZ", ci: "6287779", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-025": { nombre: "JAVIER DAVID MAMANI AUZA", ci: "5829664", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-026": { nombre: "BENITA CALIZAYA FAJARDO", ci: "3224290", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-027": { nombre: "MARTHA VACA BECERRA", ci: "4548930", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-028": { nombre: "MERCEDES PERALTA DE HUAYNOCA", ci: "3749446", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-029": { nombre: "ANA MARIA YAPU ROBLES", ci: "3190809", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-030": { nombre: "TEODORO AGUILAR", ci: "", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-031": { nombre: "FEDERICO PUMA", ci: "", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-032": { nombre: "CRISPIN CHOQUE", ci: "", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-033": { nombre: "RAUL JAILLITA", ci: "4629935", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-034": { nombre: "ANTONIO CONDORI", ci: "4678664", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-035": { nombre: "REINALDO CONDORI", ci: "8970314", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-036": { nombre: "GREGORIO NINA", ci: "5420844", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-037": { nombre: "MARCIAL COLQUE", ci: "6478665", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-038": { nombre: "EMILIO FLORES", ci: "1368032", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-039": { nombre: "LUCIO RAMIREZ", ci: "10514216", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-040": { nombre: "ESTEBAN PUMA", ci: "3238650", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-041": { nombre: "ROLI PUMA", ci: "9765396", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-042": { nombre: "ROBERTO LUCAS", ci: "6321189", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-043": { nombre: "ERNESTO MAMANI", ci: "", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-044": { nombre: "ENRIQUE GONZALES", ci: "3577235", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-045": { nombre: "DEISI GERÓNIMO", ci: "9046212", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-046": { nombre: "GERMÁN PUMA TOCO", ci: "8954452", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-047": { nombre: "WALBERTO PUMA", ci: "7809972", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-048": { nombre: "MARIO JESÚS", ci: "6384063", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-049": { nombre: "MARCO PUMA", ci: "11392707", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-050": { nombre: "ELIAS HUGARTRE", ci: "9805403", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-051": { nombre: "FELIX CHOQUE", ci: "8945878", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-052": { nombre: "ERMINIA CARDENAS", ci: "9797048", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-053": { nombre: "NICOLAS SUBIA", ci: "3197142", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-054": { nombre: "GREGORIO FLORES", ci: "7803768", distrito: "Distrito 2", estado: "Afiliado válido" },
+  "BV-D2-055": { nombre: "NICOLASA ALANIS", ci: "4637754", distrito: "Distrito 2", estado: "Afiliado válido" }
+},
+    
+    // DISTRITO 1 BUENA VISTA
+
+    
   };
